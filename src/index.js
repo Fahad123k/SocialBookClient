356ecb5c485b78b5d7fb3875a31f5c621e1cdbb7
nisu.ahmad <nisu.ahmad@samarth.ac.in>
2023-12-25 13:24:46 +0530

    add navbar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import authReducer from './state'
-import { configureStore } from '@reduxjs/toolkit';
-import { Provider } from 'react-redux';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import authReducer from "./state";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 import {
   persistStore,
   persistReducer,
@@ -14,38 +13,30 @@ import {
   PAUSE,
   PERSIST,
   PURGE,
-  REGISTER
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { PersistGate } from 'redux-persist/integration/react';
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
 
-const persistConfig={key:"root",storage,version:1}
-const persistedReducer=persistReducer(persistConfig,authReducer)
-const store=configureStore({
-  reducer:persistReducer,
-  middleware:(getDefaultMiddleware)=>{
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck:{
-        ignoreActions:[FLUSH,
-          REHYDRATE,
-          PAUSE,
-          PERSIST,
-          PURGE,
-          REGISTER]
-      }
-    })
-  }
-})
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistStore(store)}>
-    <App />
-    </PersistGate>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
-
-
